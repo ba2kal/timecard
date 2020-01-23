a78124423c22a02b66d41eb54c475d94db9b7494
ba2kal <ba2kal@naver.com>
2020-01-23 23:16:24 +0900

    bulma setting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TimeCard from "./component/TimeCard";
+require('../public/scss/mystyles.scss');
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<TimeCard/>, rootElement);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: "development",
@@ -15,14 +16,33 @@ module.exports = {
                 include:path.resolve(__dirname),
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-            }
-        ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                      loader: 'css-loader'
+                    },
+                    {
+                      loader: 'sass-loader',
+                      options: {
+                        sourceMap: true,
+                        // options...
+                      }
+                    }
+                  ]
+              }
+        ],
     },
     plugins: [
         new HtmlWebPackPlugin({
           template: './public/index.html', 
           filename: 'index.html', 
         }),
+        new MiniCssExtractPlugin({
+            filename: 'mystyles.css'
+          })
     ],
     devServer:{
         contentBase:'./public',
